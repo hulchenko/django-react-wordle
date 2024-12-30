4a91305c64fbc0a19d4afa48ff68509efe250a51
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-12-30 17:50:00 -0400

    update api endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # references applications in the project
-    path('admin/', admin.site.urls), # admin panel (default)
-    path('wordle/', include('wordle.urls')), # game(+leaderboard) app
+    path("admin/", admin.site.urls),  # admin panel (default)
+    path("api/", include("wordle.urls")),  # game(+leaderboard) app
 ]
--- backend/wordle/urls.py
+++ backend/wordle/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 urlpatterns = [
     # admin panel
-    path('users/', views.get_users, name="get_users"),
-    path('users/create', views.create_user, name="create_user"),
-
+    path("users/", views.get_users, name="get_users"),
+    path("users/create", views.create_user, name="create_user"),
     # UI interaction
-    path('leaderboard/', views.get_leaderboard, name="get_leaderboard"),
-    path('leaderboard/submit-score', views.create_leaderboard_record, name="create_leaderboard_record")
+    path("leaderboard/", views.get_leaderboard, name="get_leaderboard"),
+    path(
+        "leaderboard/submit-score",
+        views.create_leaderboard_record,
+        name="create_leaderboard_record",
+    ),
+    path("new-game/", views.new_game, name="new_game"),
+    path("submit-guess/", views.guess_word, name="guess_word"),
 ]
