c89e8c7205266e71c3234ae3cc08bbf236979604
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-12-30 17:38:18 -0400

    add game logic to views

--- backend/wordle/views.py
+++ backend/wordle/views.py
@@ -1,3 +1,7 @@
+import re
+import time
+import requests
+from sys import exit
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,14 +13,18 @@ from django.db.models import F
 
 # User methods
 
-@api_view(['GET'])
-def get_users(request): # for admin panel
-    users = get_list_or_404(User) # replaces User.objects.all(), but also throws 404 if failed to fetch
+
+@api_view(["GET"])
+def get_users(request):  # for admin panel
+    users = get_list_or_404(
+        User
+    )  # replaces User.objects.all(), but also throws 404 if failed to fetch
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
-def create_user(request): # for admin panel
+
+@api_view(["POST"])
+def create_user(request):  # for admin panel
     new_user = request.data
     serializer = UserSerializer(data=new_user)
     if serializer.is_valid():
@@ -24,27 +32,34 @@ def create_user(request): # for admin panel
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def get_user(request): # called from leaderboard POST
+
+def get_user(request):  # called from leaderboard POST
     # Check if user already exists, if not - create one
-    user_email = request.data.get('user_email')
-    user_name = request.data.get('user_name')
+    user_email = request.data.get("user_email")
+    user_name = request.data.get("user_name")
     user, is_new_user = User.objects.get_or_create(email=user_email)
     if is_new_user:
         user.name = user_name
-        user.save() # store in DB
+        user.save()  # store in DB
     return user
+
 
 # ---
 
+
 # Leaderboard methods
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_leaderboard(request):
-    leaderboard = get_list_or_404(Leaderboard) # replaces Leaderboard.objects.all(); if empty -> "detail": "No Leaderboard matches the given query."
+    leaderboard = get_list_or_404(
+        Leaderboard
+    )  # replaces Leaderboard.objects.all(); if empty -> "detail": "No Leaderboard matches the given query."
     serializer = LeaderboardSerializer(leaderboard, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def create_leaderboard_record(request):
     # expected frontend object
     # {
@@ -52,9 +67,6 @@ def create_leaderboard_record(request):
     #   "user_email": "test@example.com",
     #   "score": 1000
     # }
-
-    print("CREATE LEADERBOARD DATA: ", request.data)
-
     user = get_user(request)
 
     # Get/create leaderboard record
@@ -63,13 +75,140 @@ def create_leaderboard_record(request):
     if is_new_record:
         leaderboard_record.games_played = 1
     else:
-        leaderboard_record.games_played = F('games_played') + 1 # F is a built-in function for manipulating the existing DB data
-    leaderboard_record.save() # store in DB
+        leaderboard_record.games_played = (
+            F("games_played") + 1
+        )  # F is a built-in function for manipulating the existing DB data
+    leaderboard_record.save()  # store in DB
 
     # Refresh from DB to get updated games_played count
     leaderboard_record.refresh_from_db()
 
     serializer = LeaderboardSerializer(instance=leaderboard_record)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# ---
+
+# Game logic
+
+
+def fetch_random_word():
+    word_length = 5
+    try:
+        random_word = requests.get(
+            f"https://random-word-api.herokuapp.com/word?length={word_length}",
+            timeout=5,
+        )
+        random_word.raise_for_status()  # throw an error on 4xx/5xx status code
+        return random_word.json().pop().lower()
+    except requests.exceptions.RequestException as error:
+        print("Fetch failed: ", error)
+        return Response({"message": "Error getting word"})
+
+
+def validate_input(input):
+    if len(input) != 5:
+        return {"ok": False, "message": "5-letter word only"}
+    if not re.match(r"^[a-z]+$", input):
+        return {"ok": False, "message": "Alphabetic letters only"}
+    else:
+        return {"ok": True}
+
+
+def highlight_user_input(input, word):
+    def mark_green(letter):
+        return {"letter": letter, "color": "green"}
+
+    def mark_yellow(letter):
+        return {"letter": letter, "color": "yellow"}
+
+    def mark_gray(letter):
+        return {"letter": letter, "color": "gray"}
+
+    result = [""] * len(word)
+    word_array = list(word)
+    tracking_letters = list(word_array)  # create copy for tracking
+
+    for i in range(len(word_array)):
+        if input[i] == word_array[i]:
+            result[i] = mark_green(input[i])
+            tracking_letters[i] = None  # use in mapping & remove from tracking
+    for i in range(len(word_array)):
+        if result[i] == "":  # go through the unmarked letters
+            if input[i] in tracking_letters:
+                result[i] = mark_yellow(input[i])
+                marked_char_idx = tracking_letters.index(input[i])
+                tracking_letters[marked_char_idx] = None  # remove from tracking
+            else:
+                result[i] = mark_gray(input[i])
+    return result
+
+
+@api_view(["GET"])
+def new_game(request):
+    random_word = fetch_random_word()
+    # store session variables
+    request.session["word"] = random_word
+    request.session["attempts"] = 6
+    request.session["words_grid"] = []
+    return Response({"message": "Game started", "attempts": 6})
+
+
+@api_view(["POST"])
+def guess_word(request):
+    # get session variables
+    word = request.session.get("word")
+    print("WORD:", word)  # TODO remove
+    attempts = request.session.get("attempts", 0)
+    words_grid = request.session.get("words_grid")
+    if attempts <= 0:
+        return Response({"message": "Game over!"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user_input = request.data.get("guess")
+    is_input_valid = validate_input(user_input)
+    if not is_input_valid["ok"]:
+        return Response(
+            {"message": is_input_valid["message"]}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    if user_input != word:
+        # incorrect guess
+        if user_input not in words_grid:
+            # new word guess
+            attempts = attempts - 1
+            words_grid.append(user_input)
+            highlighted_input = highlight_user_input(user_input, word)
+
+            # update session variables
+            request.session["attempts"] = attempts
+            request.session["words_grid"] = words_grid
+            return Response(
+                {"message": None, "result": highlighted_input, "attempts": attempts}
+            )
+        else:
+            return Response(
+                {
+                    "message": "The word is already in the list!",
+                    "result": None,
+                    "attempts": attempts,
+                }
+            )
+    else:
+        # correct guess
+        attempts = attempts - 1
+        words_grid.append(user_input)
+        highlighted_input = highlight_user_input(user_input, word)
+
+        # update session variables
+        request.session["attempts"] = attempts
+        request.session["words_grid"] = words_grid
+        return Response(
+            {
+                "message": "Congratulations, you won!",
+                "result": highlighted_input,
+                "attempts": attempts,
+            }
+        )
+
 
 # ---
