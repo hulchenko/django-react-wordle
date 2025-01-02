a412139294228371b1cf26cfc4251ce2ea2302a2
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-01-01 20:46:06 -0400

    improve footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div className="w-full absolute bottom-0 border border-t-gray-300 font-thin text-sm">
+      <a href="https://github.com/hulchenko" target="_blank">
+        hulchenko
+      </a>
+    </div>
+  );
+};
