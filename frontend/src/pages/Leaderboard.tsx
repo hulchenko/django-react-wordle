f2cfba3bd226e580755a725c894d6b264870b0e2
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-01-02 18:19:01 -0400

    add leaderboard page

---
+++ frontend/src/pages/Leaderboard.tsx
@@ -0,0 +1,72 @@
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "../components/Loader";
+import { Error } from "../components/Error";
+
+const fetchLeaderboardData = () => fetch("/api/leaderboard/").then((res) => res.json());
+
+export const Leaderboard = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["leaderboardData"],
+    queryFn: fetchLeaderboardData,
+  });
+
+  interface UserInfo {
+    id: number;
+    user_name: string;
+    user_email: string;
+    score: number;
+    score_date: string;
+    wins: number;
+  }
+
+  const dateDisplay = (date: string) => {
+    if (!date) {
+      return null;
+    }
+    const month = new Date(date).toLocaleString("en-CA", { month: "long" });
+    const day = new Date(date).getDate();
+    const hours = new Date(date).getHours();
+    const minutes = new Date(date).getMinutes();
+
+    return `${month} ${day}, ${hours}:${minutes}`;
+  };
+
+  if (isPending) return <Loader marginTop={96} size={30} />;
+  if (error) return <Error />;
+
+  return (
+    <div className="mt-40">
+      <h1 className="mb-4 text-2xl font-bold">Leaderboard 🏆</h1>
+      <table className="m-auto bg-white border border-slate-300 text-start inline-block max-h-[600px] overflow-y-auto">
+        <thead>
+          <tr className="bg-slate-100">
+            <th className="py-2 px-4 border-b">Name</th>
+            <th className="py-2 px-4 border-b">Email</th>
+            <th className="py-2 px-4 border-b">Score</th>
+            <th className="py-2 px-4 border-b">Submitted On</th>
+            <th className="py-2 px-4 border-b">Wins</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.length > 0 ? (
+            data.map((item: UserInfo) => (
+              <tr key={item.id} className="hover:bg-slate-50">
+                <td className="py-2 px-4 border-b max-w-96 overflow-hidden text-ellipsis">{item.user_name || "N/A"}</td>
+                <td className="py-2 px-4 border-b max-w-96 overflow-hidden text-ellipsis">{item.user_email || "N/A"}</td>
+                <td className="py-2 px-4 border-b">{item.score || "N/A"}</td>
+                <td className="py-2 px-4 border-b">{dateDisplay(item.score_date) || "N/A"}</td>
+                <td className="py-2 px-4 border-b">{item.wins || "N/A"}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={5} className="py-4 text-center">
+                Nothing here yet.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
