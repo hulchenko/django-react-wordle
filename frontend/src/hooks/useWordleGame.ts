e7b9838861f4a9ea1a2662a381e92b233965c09b
Vadym Hulchenko <vadymhulchenko@gmail.com>
2025-01-03 15:46:27 -0400

    code improvements: create useWordleGame hook, separate game logic from the component

---
+++ frontend/src/hooks/useWordleGame.ts
@@ -0,0 +1,159 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useCallback, useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
+import { MAX_ATTEMPTS, WORD_LENGTH } from "../constants/contstants";
+import { Cell, Grid, GameStart } from "../interfaces/GameBoard";
+
+const startGame = (): Promise<GameStart> => fetch("api/new-game/").then((res) => res.json());
+const defaultCell = { letter: "", color: "", local: true };
+const initGameState = { over: false, victory: false, message: "", score: 0, target: "" };
+
+export const useWordleGame = () => {
+  const [gameKey, setGameKey] = useState(Date.now()); // required for new game
+  const [grid, setGrid] = useState<Grid>(Array(MAX_ATTEMPTS).fill(Array(WORD_LENGTH).fill({ ...defaultCell })));
+  const [gameInfo, setGameInfo] = useState({ ...initGameState });
+  const [attempts, setAttempts] = useState(0);
+  const [disabledInput, setDisabledInput] = useState(false);
+
+  const userGuessStr = useRef("");
+  const userGuessArr = useRef<Cell[]>(Array.from({ length: WORD_LENGTH }, () => ({ ...defaultCell })));
+
+  const {
+    error,
+    isLoading,
+    data: gameData,
+  } = useQuery({
+    queryKey: ["newGame", gameKey],
+    queryFn: startGame,
+  });
+
+  const submitGuess = useMutation({
+    mutationFn: async (guess: string) => {
+      try {
+        const response = await fetch("api/submit-guess/", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ guess }),
+        });
+        const data = await response.json();
+        if (!response.ok) {
+          // intercept + throw error for onError() method, otherwise it ends up in onSuccess()
+          throw data.error;
+        }
+        return data;
+      } catch (error) {
+        throw error;
+      }
+    },
+    onSuccess: (data) => {
+      console.log(`ON SUCCESS: `, data);
+      const { error, attempts: newAttempts, result, victory, score, target } = data;
+      if (error) {
+        // intercept for onError()
+        throw error;
+      }
+      if ("victory" in data) {
+        setGameInfo((prevInfo) => ({ ...prevInfo, over: true, victory, score, target }));
+      }
+      setGrid((prevGrid) => {
+        const newGrid = [...prevGrid];
+        newGrid[MAX_ATTEMPTS - attempts] = result.map((letter: string) => letter); // update current array sequence
+        return newGrid;
+      });
+      setAttempts(newAttempts);
+      resetLocalInput();
+      setDisabledInput(false);
+    },
+    onError: (error: string) => {
+      console.error("Error submitting guess: ", error);
+      toast.error(error);
+      resetLocalInput(error);
+      setDisabledInput(false);
+    },
+  });
+
+  const resetLocalInput = (error: string = "") => {
+    const blankInputArr = Array.from({ length: WORD_LENGTH }, () => ({ ...defaultCell }));
+    userGuessArr.current = blankInputArr;
+    userGuessStr.current = "";
+    if (error) {
+      setGrid((prevGrid) => {
+        const newGrid = [...prevGrid];
+        newGrid[MAX_ATTEMPTS - attempts] = blankInputArr; // reset current input array
+        return newGrid;
+      });
+    }
+  };
+
+  const keyPressHandler = useCallback(
+    (key: string) => {
+      if (gameInfo.over) return;
+
+      const alphabetical = /^[A-Z]$/;
+      const guessArr = userGuessArr.current;
+
+      const updateGrid = () => {
+        setGrid((prevGrid) => {
+          const newGrid = [...prevGrid];
+          newGrid[MAX_ATTEMPTS - attempts] = guessArr; // update current array sequence
+          return newGrid;
+        });
+        const guessStr = guessArr
+          .map((slot) => slot.letter)
+          .join("")
+          .toLowerCase();
+        userGuessStr.current = guessStr;
+      };
+
+      if (alphabetical.test(key) && !disabledInput) {
+        const emptySlot = guessArr.findIndex((slot) => slot.letter === ""); // fill out the first empty string
+        if (emptySlot !== -1) {
+          guessArr[emptySlot].letter = key;
+          guessArr[emptySlot].color = "input";
+          updateGrid();
+        }
+      } else if (key === "BACKSPACE") {
+        for (let i = guessArr.length - 1; i >= 0; i--) {
+          if (guessArr[i].letter !== "") {
+            // clear out the last filled string
+            guessArr[i].letter = "";
+            guessArr[i].color = "";
+            updateGrid();
+            break;
+          }
+        }
+      } else if (key === "ENTER") {
+        if (userGuessStr.current.length === WORD_LENGTH) {
+          setDisabledInput(true);
+          submitGuess.mutate(userGuessStr.current);
+        } else {
+          toast.error("5-letter word only!");
+        }
+      }
+    },
+    [attempts, disabledInput, gameInfo, submitGuess]
+  );
+
+  const restartGame = useCallback(() => {
+    setGrid(Array(MAX_ATTEMPTS).fill(Array(WORD_LENGTH).fill({ ...defaultCell })));
+    setGameInfo({ ...initGameState });
+    setGameKey(Date.now()); // refetch
+  }, []);
+
+  useEffect(() => {
+    if (gameData) {
+      // initialize game data from query call
+      console.log(gameData);
+      const { message, attempts } = gameData;
+      toast(message);
+      setAttempts(attempts);
+      setGameInfo({ ...initGameState });
+      setGrid(Array(MAX_ATTEMPTS).fill(Array(WORD_LENGTH).fill({ ...defaultCell })));
+    }
+    if (error) {
+      toast.error("Error connecting to the game server.");
+    }
+  }, [gameData, error]);
+
+  return { grid, gameInfo, isLoading, error, keyPressHandler, restartGame };
+};
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,187 +1,36 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
+import { useEffect } from "react";
 import { Error } from "../components/Error";
 import { Loader } from "../components/Loader";
 import { ScoreModal } from "../components/ScoreModal";
-
-const customColors = {
-  gray: "bg-slate-600",
-  yellow: "bg-yellow-500",
-  green: "bg-green-600",
-  input: "bg-slate-400",
-};
-
-const startGame = () => fetch("api/new-game/").then((res) => res.json());
+import { COLORS } from "../constants/contstants";
+import { useWordleGame } from "../hooks/useWordleGame";
 
 export const Game = () => {
-  const [gameKey, setGameKey] = useState(Date.now()); // required for new game
-  const [grid, setGrid] = useState(Array(6).fill(Array(5).fill("")));
-  const [gameInfo, setGameInfo] = useState({ over: false, victory: false, message: "", score: 0, target: "" });
-  const [attempts, setAttempts] = useState(0);
-  const [disabledInput, setDisabledInput] = useState(false);
-
-  const userGuess = useRef("");
-
-  const {
-    error,
-    isLoading,
-    data: gameData,
-  } = useQuery({
-    queryKey: ["newGame", gameKey],
-    queryFn: startGame,
-  });
+  const { grid, gameInfo, isLoading, error, keyPressHandler, restartGame } = useWordleGame();
 
   useEffect(() => {
-    if (gameData) {
-      // initialize game data from query call
-      console.log(gameData);
-      const { message, attempts } = gameData;
-      toast(message);
-      setAttempts(attempts);
-      setGameInfo({ over: false, victory: false, message: "", score: 0, target: "" });
-      setGrid(Array(6).fill(Array(5).fill("")));
-    }
-    if (error) {
-      toast.error("Error connecting to the game server.");
-    }
-  }, [gameData, error]);
-
-  useEffect(() => {
-    const alphabetical = /^[A-Z]$/;
-    let guessArr = Array.from({ length: 5 }, () => ({ letter: "", color: "", local: true })); // .fill() didn't work, as it points to the same object in memory
-
-    const guessHandler = (e) => {
-      const key = e.key.toUpperCase();
-
-      const updateGrid = () => {
-        setGrid((prevGrid) => {
-          const newGrid = [...prevGrid];
-          newGrid[6 - attempts] = guessArr; // update current array sequence
-          return newGrid;
-        });
-        const guessStr = guessArr
-          .map((slot) => slot.letter)
-          .join("")
-          .toLowerCase();
-        userGuess.current = guessStr;
-      };
-
-      if (alphabetical.test(key) && !disabledInput) {
-        const emptySlot = guessArr.findIndex((slot) => slot.letter === ""); // fill out the first empty string
-        if (emptySlot !== -1) {
-          guessArr[emptySlot].letter = key;
-          guessArr[emptySlot].color = "input";
-          updateGrid();
-        }
-      } else if (key === "BACKSPACE") {
-        for (let i = guessArr.length - 1; i >= 0; i--) {
-          if (guessArr[i].letter !== "") {
-            // clear out the last filled string
-            guessArr[i].letter = "";
-            guessArr[i].color = "";
-            updateGrid();
-            break;
-          }
-        }
-      } else if (key === "ENTER") {
-        if (gameInfo.over) return;
-        if (userGuess.current.length === 5) {
-          setDisabledInput(true);
-          submitGuess.mutate(userGuess.current);
-
-          // reset values
-          userGuess.current = "";
-          guessArr = Array.from({ length: 5 }, () => ({ letter: "", color: "", local: true }));
-        } else {
-          toast.error("5-letter word only!");
-        }
-      }
-    };
-
-    if (!gameInfo.over || attempts > 0) {
-      document.addEventListener("keyup", guessHandler);
-    }
-
-    return () => {
-      document.removeEventListener("keyup", guessHandler);
-    };
-  }, [attempts, disabledInput]);
-
-  const submitGuess = useMutation({
-    mutationFn: async (guess: string) => {
-      try {
-        const response = await fetch("api/submit-guess/", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ guess }),
-        });
-        const data = await response.json();
-        if (!response.ok) {
-          // intercept + throw error for onError() method, otherwise it ends up in onSuccess()
-          throw data.error;
-        }
-        return data;
-      } catch (error) {
-        throw error;
-      }
-    },
-    onSuccess: (data) => {
-      console.log(`ON SUCCESS: `, data);
-      const { error, attempts: newAttempts, result, victory, score, target } = data;
-      if (error) {
-        // intercept for onError()
-        throw error;
-      }
-      if ("victory" in data) {
-        setGameInfo((prevInfo) => ({ ...prevInfo, over: true, victory, score, target }));
-      }
-      setGrid((prevGrid) => {
-        const newGrid = [...prevGrid];
-        newGrid[6 - attempts] = result.map((letter: string) => letter); // update current array sequence
-        return newGrid;
-      });
-      setAttempts(newAttempts);
-      setDisabledInput(false);
-    },
-    onError: (error: string) => {
-      console.error("Error submitting guess: ", error);
-      toast.error(error);
-
-      // TODO add global function to reset current grid (use in useState gameData too)
-      const guessArr = Array.from({ length: 5 }, () => ({ letter: "", color: "", local: true }));
-      setGrid((prevGrid) => {
-        const newGrid = [...prevGrid];
-        newGrid[6 - attempts] = guessArr; // update current array sequence
-        return newGrid;
-      });
-      setDisabledInput(false);
-    },
-  });
-
-  const restart = () => {
-    setGrid(Array(6).fill(Array(5).fill("")));
-    setGameInfo({ over: false, victory: false, message: "", score: 0, target: "" });
-    setGameKey(Date.now()); // refetch
-  };
+    const press = (e: KeyboardEvent) => keyPressHandler(e.key.toUpperCase());
+    document.addEventListener("keyup", press);
+    return () => document.removeEventListener("keyup", press);
+  }, [keyPressHandler]);
 
   if (isLoading) return <Loader marginTop={96} size={30} />;
   if (error) return <Error />;
 
   return (
     <>
-      {gameInfo.over && <ScoreModal victory={gameInfo.victory} restart={restart} score={gameInfo.score} target={gameInfo.target} />}
+      {gameInfo.over && <ScoreModal victory={gameInfo.victory} restart={restartGame} score={gameInfo.score} target={gameInfo.target} />}
       {!gameInfo.over && (
         <div className="max-w-[600px] h-[600px] m-auto flex flex-col gap-1 mt-40 border border-slate-300 p-2 rounded bg-slate-100 animate-fade-in">
           {grid.map((row, i) => (
             <div key={i} className="flex grow gap-1">
-              {row.map((cell: { letter: string; color: string }, j: number) => (
+              {row.map((cell, j: number) => (
                 <div
                   key={j}
                   className={`w-full flex justify-center items-center text-2xl uppercase font-bold text-white min-h-[40px] rounded 
                   ${cell.letter && "animate-boop"}
                   ${cell && !cell.local && "animate-flip"}
-                  ${customColors[cell.color] || "bg-slate-300"}`}
+                  ${COLORS[cell.color] || "bg-slate-300"}`}
                 >
                   {cell.letter || null}
                 </div>
